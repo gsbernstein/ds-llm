aed382880a1b4d1d9508aa5dae3781f3b62b7920
Greg Bernstein <gsbernst@gmail.com>
2025-08-17 21:44:45 -0700

    fixup local

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -6,7 +6,7 @@ import {
   SampleTranscriptResponse 
 } from '../types';
 
-const API_BASE_URL = process.env['REACT_APP_API_URL'] || '/api';
+const API_BASE_URL = process.env['REACT_APP_API_URL'] || 'http://localhost:5001/api';
 
 export class ApiService {
   /**
